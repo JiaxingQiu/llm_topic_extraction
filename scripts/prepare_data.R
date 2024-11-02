60dbfd8d4be420b16ce73c74ddfd4933c44ac336
JiaxingQiu <jiaxingq1996@gmail.com>
2024-11-02 18:15:51 -0400

    upload data and code

---
+++ scripts/prepare_data.R
@@ -0,0 +1,18 @@
+
+# 1. answer_df
+# run "~/Documents/Documents JoyQiu Work/Research/ED Media/network/script/llm/data_clean_in_r_with_EOS.R" first
+df_llm <- read.csv("/Users/joyqiu/Documents/Documents JoyQiu Work/Research/ED Media/network/./script/llm/sm_eos.csv", stringsAsFactors = F)
+
+answer_df <- df_llm[,c("sm_id", "text_w_eos")]
+
+write.csv(answer_df, "/Users/joyqiu/Documents/Documents JoyQiu Work/Research/LLMTopicExtraction/llm_topic_extraction/data/answer_df_raw.csv", row.names = F)
+
+
+
+# 2. fea_df
+# run "~/Documents/Documents JoyQiu Work/Research/ED Media/network/script/llm/prepare_fea_df.R"
+source("~/Documents/Documents JoyQiu Work/Research/ED Media/network/script/llm/prepare_fea_df.R")
+write.csv(fea_df, "/Users/joyqiu/Documents/Documents JoyQiu Work/Research/LLMTopicExtraction/llm_topic_extraction/data/fea_df.csv", row.names = F)
+
+
+
